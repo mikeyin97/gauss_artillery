f668cb00e485c43f5134cf2f9c861e5531da25bb
hschier <hudsonschier@gmail.com>
2018-03-16 20:22:13 -0400

    Basic canon firing code

---
+++ src/functions.cpp
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include <functions.h>
+
+int add(int a, int b) {
+	return a + b; // test func
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <Arduino.h>
+#include <functions.h>
 #include <../lib/AccelStepper.h>
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);
+  Serial.println("Done Setup");
+  add(1, 2); // test function
 }
 
 void loop() {
-    Serial.println("f");
+  while (Serial.available() > 0) {
+    int firetime = Serial.parseInt();
+    Serial.println(firetime);
+    if (Serial.read() == '\n') {
+      Serial.println(firetime);
+      digitalWrite(8, HIGH);
+      delayMicroseconds(firetime);
+      digitalWrite(8, LOW);
+    }
+  }
 }
