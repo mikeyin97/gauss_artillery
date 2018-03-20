8cd6b10d1cba38ae6e9e666be90c6177f454a7cf
OsamaL <lodhi_osama@hotmail.com>
2018-03-20 16:35:27 -0400

    hi

--- src/hudsons_code/hudsons_code.ino
+++ src/hudsons_code/hudsons_code.ino
@@ -3,21 +3,27 @@
 // #include <../lib/AccelStepper.h>
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(8, OUTPUT);
+  pinMode(5, OUTPUT);
   Serial.println("Done Setup");
   //add(1, 2); // test function
 }
 
 void loop() {
-  float division = 1.0;
+  float division = 19.0;
   float volt_factor = 5.0/1023.0;
-  Serial.println(analogRead(A0) * division * volt_factor);
+  float voltage = 0;
   while (Serial.available() > 0) {
-    Serial.println(analogRead(A0) * division * volt_factor);
     int firetime = Serial.parseInt();
-    Serial.println(firetime);
     if (Serial.read() == '\n') {
+      digitalWrite(5 ,HIGH);
+      do {
+        voltage = analogRead(A0) * division * volt_factor;
+        Serial.println(voltage);
+      } while(voltage > 68.0);
+      digitalWrite(5, LOW);
+      Serial.println("FIRING");
       Serial.println(firetime);
       digitalWrite(8, HIGH);
       delayMicroseconds(firetime);
