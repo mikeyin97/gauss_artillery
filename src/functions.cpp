bdbc95926ae52bf76ca9d5843488cf2fed0e0081
Ted <teddy57320@gmail.com>
2018-03-25 18:01:15 -0400

    Move cannon code to function

--- src/functions.cpp
+++ src/functions.cpp
@@ -7,6 +7,28 @@ int add(int a, int b) {
 
 float cannonVoltage() {
 	float division = 22.15;
-  	float volt_factor = 5.0/1024.0;
+		float volt_factor = 5.0/1024.0;
 	return (float) analogRead(A0) * division * volt_factor;
+}
+
+void cannonTest() {
+	float division = 19.0;
+	float volt_factor = 5.0/1023.0;
+	float voltage = 0;
+	while (Serial.available() > 0) {
+	int firetime = Serial.parseInt();
+	if (Serial.read() == '\n') {
+		digitalWrite(drain_pin, HIGH);
+		do {
+            voltage = cannonVoltage();
+            Serial.println(voltage);
+		} while(voltage > 66.0);
+            digitalWrite(discharge_pin, LOW);
+            Serial.println("FIRING");
+            Serial.println(firetime);
+            digitalWrite(cannon_pin, HIGH);
+            delayMicroseconds(firetime);
+            digitalWrite(cannon_pin, LOW);
+	    }
+	}
 }
--- src/functions.h
+++ src/functions.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 #include <Arduino.h>
 
+const uint8_t cannon_pin = 5;
+const uint8_t discharge_pin = 8;
+
 int add(int a, int b);
 float cannonVoltage();
+void cannonTest();
 
 #endif
